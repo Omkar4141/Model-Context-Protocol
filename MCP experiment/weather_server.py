1209c7f34dd95295f2cead5705d1664012cf65f1
Omkar Rajendra Lokhande <77824136+Omkar4141@users.noreply.github.com>
2025-08-23 14:40:44 +0530

    Update weather_server.py

--- MCP experiment/weather_server.py
+++ MCP experiment/weather_server.py
@@ -1,5 +1,6 @@
 from mcp.server.fastmcp import FastMCP
 mcp=FastMCP("Weather")
+# you can define your functions here that are realated to weather
 @mcp.tool()
 async def get_weather(location:str)->str:
     return f"{location}:sunny-23 degeee"
